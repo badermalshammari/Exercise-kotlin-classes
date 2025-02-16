983ce5fbe0dd43cee604cb1a52112b34f848d4d4
Bader Alshammari <q8.badoori.11@gmail.com>
2025-02-16 18:40:17 +0300

    Exercise-kotlin-classes

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
+class Book(var Title: String, var Author: String, var yearPublished: Int) {
+    var genre: String = ""
+    fun getBookInfo(): String {
+        if (genre == "") {
+            return "$Title by $Author ($yearPublished)"
+        }
+        return "$Title by $Author $genre ($yearPublished)"
+    }
+
+}
+
+
 fun main() {
-    println("Hello World!")
+    var book = Book("Life","Bader",2025)
+    book.genre = "Horror"
+    println(book.getBookInfo())
 }
